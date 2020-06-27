b58e9dc4218248aaba5b0c75842745083d015562
shadow8evil@gmail.com <shadow8evil@gmail.com>
2020-06-27 18:46:19 +0300

    trying

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 /* if scrolling add the background color to the navbar */
 let myNav = document.querySelector('#myNav');
 
-// window.onscroll = function() {
-//     "use strict";
-//     if (document.body.scrollTop >= 350 || document.documentElement.scrollTop >= 350) {
-//         myNav.classList.add("bg-custom");
-//         myNav.classList.remove("bg-transparent");
-//     } else {
-//         myNav.classList.add("bg-transparent");
-//         myNav.classList.remove("bg-custom");
-//     }
-// };
+window.onscroll = function() {
+    "use strict";
+    if (document.body.scrollTop >= 350 || document.documentElement.scrollTop >= 350) {
+        myNav.classList.add("bg-custom");
+        myNav.classList.remove("bg-transparent");
+    } else {
+        myNav.classList.add("bg-transparent");
+        myNav.classList.remove("bg-custom");
+    }
+};
 
 /* if the screen height is below 550 and you hover over the bar icon the navbar will get background if it was transparent */
 myNav.addEventListener('mousemove', function() {
